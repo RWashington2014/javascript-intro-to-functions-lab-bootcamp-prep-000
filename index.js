c017195fc2fcb36b4004243dce0be35b42873780
Robert Washington <robwash@gmail.com>
2017-08-21 23:19:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function sayHiToGrandma(string){
   else if (string.toUpperCase() === string) {
     return ("YES INDEED")
   }
-  else ('I Love you, Grandma.'=== string){
+  else ('I Love you, Grandma.'=== string)
     return ("I love you, too.")
-}
 }
